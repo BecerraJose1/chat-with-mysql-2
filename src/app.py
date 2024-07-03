ad22dd7f6b5fc87a85a21a17c5ba39f3e149973f
BecerraJose1 <becerrajose037@gmail.com>
2024-07-03 16:42:46 -0500

    "Added database connection functionality and updated Streamlit layout and inputs"

--- src/app.py
+++ src/app.py
@@ -1,10 +1,15 @@
 from dotenv import load_dotenv
+from langchain_community.utilities import SQLDatabase
 import streamlit as st
 
 
+def init_database(user:str, password:str, host:str, port: str, database:str) -> SQLDatabase:
+    db_uri =f"mysql+mysqlconnector://{user}:{password}:@{host}:{port}/{database}"
+    return SQLDatabase.from_uri(db_uri)
+
 load_dotenv()
 
-st.set_page_config(page_title="Chat with Hootsi",page_icon=" :speech_balloon:" )
+st.set_page_config(page_title="Chat with Hootsi",page_icon=":speech_balloon:", layout="wide", initial_sidebar_state="expanded")
 
 st.title("Chat with Hootsi")
 
@@ -12,10 +17,22 @@ with st.sidebar:
     st.subheader("Settings")
     st.write("This is a chat application using MySQL for Hootsi.")
 
-    st.text_input("Host", value="localhost")
-    st.text_input("Port", value="3306")
-    st.text_input("User", value="root")
-    st.text_input("Password", type="password", value="example")
-    st.text_input("Database", value="DB")
+    st.text_input("Host", value="localhost", key="Host")
+    st.text_input("Port", value="3306", key="Port")
+    st.text_input("User", value="root", key="User")
+    st.text_input("Password", type="password", value="example", key="Password")
+    st.text_input("Database", value="DB", key="Database")
 
-    st.button("Connect")
+    if st.button("Connect"):
+        with st.spinner("Connecting to the DB..."):
+            db = init_database(
+                st.session_state["User"],
+                st.session_state["Password"],
+                st.session_state["Host"],
+                st.session_state["Port"],
+                st.session_state["Database"]
+            )
+            st.session_state.db = db
+            st.success("Connected!")
+
+st.chat_input("Type a message...")
