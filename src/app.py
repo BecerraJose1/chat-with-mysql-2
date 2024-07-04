8d51ab6e8436cad984705db064abfed939042bd5
BecerraJose1 <becerrajose037@gmail.com>
2024-07-03 19:35:15 -0500

    "Added SQL chain functionality, chat history, and AI response to app.py"

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv  # Import dotenv for managing environment variables
+from langchain_core.messages import AIMessage, HumanMessage
 from langchain_community.utilities import SQLDatabase  # Import SQLDatabase from langchain_community module
 import streamlit as st  # Import Streamlit for building the web application
 
@@ -18,6 +19,38 @@ def init_database(user:str, password:str, host:str, port: str, database:str) -> 
     """
     db_uri = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}"
     return SQLDatabase.from_uri(db_uri)  # Connects to the database using the URI and returns the SQLDatabase object
+
+def get_sql_chain(db):
+    template = template = """
+    You are a data analyst at a company. You are interacting with a user who is asking you questions about the company's database.
+    Based on the table schema below, write a SQL query that would answer the user's question. Take the conversation history into account.
+    
+    <SCHEMA>{schema}</SCHEMA>
+    
+    Conversation History: {chat_history}
+    
+    Write only the SQL query and nothing else. Do not wrap the SQL query in any other text, not even backticks.
+    
+    For example:
+    Question: which 3 artists have the most tracks?
+    SQL Query: SELECT ArtistId, COUNT(*) as track_count FROM Track GROUP BY ArtistId ORDER BY track_count DESC LIMIT 3;
+    Question: Name 10 artists
+    SQL Query: SELECT Name FROM Artist LIMIT 10;
+    
+    Your turn:
+    
+    Question: {question}
+    SQL Query:
+    """
+
+if "chat_history" not in st.session_state:
+
+    st.session_state.chat_history = [
+        AIMessage(content="Hello! I am a chatbot. What can I help you with?"),  # Initialize the chat history with a greeting message
+    ]
+
+
+
 
 load_dotenv()  # Loads environment variables from a .env file if present
 
@@ -56,4 +89,24 @@ with st.sidebar:  # Defines content for the sidebar
             st.session_state.db = db  # Stores the database connection object in session state
             st.success("Connected!")  # Displays success message once connected
 
-st.chat_input("Type a message...")  # Displays an input box for typing messages in the main app section
+for message in st.session_state.chat_history:
+    if isinstance(message, AIMessage):
+        with st.chat_message("AI"):
+            st.markdown(message.content)
+    elif isinstance(message, HumanMessage):
+        with st.chat_message("Human"):
+            st.markdown(message.content)
+
+user_query = st.chat_input("Type a message...")  # Displays an input box for typing messages in the main app section
+
+if user_query is not None and user_query.strip() != "":
+    st.session_state.chat_history.append(HumanMessage(content=user_query))
+
+    with st.chat_message("Human"):
+        st.markdown(user_query)
+
+
+    with st.chat_message("AI"):
+        response = "Test message"
+        st.markdown(response)
+    st.session_state.chat_history.append(AIMessage(content=response))
